474b5cf6db1ff2ce5fe9483fc201740324b45d36
Till Adam <till@kdab.com>
2013-01-19 15:26:36 +0100

    The patch for case sensitive mac file systems is now upstream, use that.

--- Library/Formula/qt-mobility.rb
+++ Library/Formula/qt-mobility.rb
@@ -2,15 +2,10 @@ require 'formula'
 
 class QtMobility < Formula
   homepage 'http://qt.nokia.com/products/qt-addons/mobility'
-  url 'git://gitorious.org/qt-mobility/qt-mobility.git', :revision => '1ccbeab'
-  version '1.2.0-1ccbeab'
+  url 'git://gitorious.org/qt-mobility/qt-mobility.git', :revision => '251b458'
+  version '1.2.0-251b458'
 
   depends_on 'qt'
-
-  def patches
-    # Fixes build on case sensitive filesystems
-    'http://lists.qt.nokia.com/pipermail/qt-mobility-feedback/attachments/20120220/be90dd45/attachment.obj'
-  end
 
   def install
     system "./configure", "-release", "-prefix", prefix, "-qmake-exec", "#{HOMEBREW_PREFIX}/bin/qmake"
