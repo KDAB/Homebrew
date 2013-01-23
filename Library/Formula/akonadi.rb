463566850cd4d6636ab18d36f652638058d2bf75
Till Adam <till@kdab.com>
2013-01-23 16:23:49 +0100

    Bump to 1.9.

--- Library/Formula/akonadi.rb
+++ Library/Formula/akonadi.rb
@@ -2,18 +2,12 @@ require 'base_kde_formula'
 
 class Akonadi < BaseKdeFormula
   homepage 'http://pim.kde.org/akonadi/'
-  url 'ftp://ftp.kde.org/pub/kde/stable/akonadi/src/akonadi-1.7.1.tar.bz2'
-  md5 '54e0556d73c22c7b3393069071e0b717'
+  url 'ftp://ftp.kde.org/pub/kde/stable/akonadi/src/akonadi-1.9.0.tar.bz2'
+  md5 '51ef4c9455f97ce4346c5934174b9da8'
 
   depends_on 'shared-mime-info'
   depends_on 'soprano'
   depends_on 'boost'
   depends_on 'qt'
 
-  def patches
-    {:p0 => [
-      "http://bugsfiles.kde.org/attachment.cgi?id=69519"
-    ]}
-  end
-  
 end
