5b3753f800c08a63cfdefdc1c7cba3db87accaf7
Till Adam <till@kdab.com>
2013-01-23 17:48:15 +0100

    attica: Bump version to 0.4.1.

--- Library/Formula/attica.rb
+++ Library/Formula/attica.rb
@@ -2,6 +2,6 @@ require 'base_kde_formula'
 
 class Attica < BaseKdeFormula
   homepage 'http://www.kde.org/'
-  url 'ftp://ftp.kde.org/pub/kde/stable/attica/attica-0.3.0.tar.bz2'
-  md5 '5a77f678422e7a52e3a3abb2d7ad0499'
+  url 'ftp://ftp.kde.org/pub/kde/stable/attica/attica-0.4.1.tar.bz2'
+  md5 'b90983ec5d79e5ddcbc9146fa23cab72'
 end
