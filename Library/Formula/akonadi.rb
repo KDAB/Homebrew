ce12073c581c051c19e65285df4060bd50770e1e
Till Adam <till@kdab.com>
2013-01-25 18:16:23 +0100

    Properly depend on cmake.

--- Library/Formula/akonadi.rb
+++ Library/Formula/akonadi.rb
@@ -5,6 +5,7 @@ class Akonadi < BaseKdeFormula
   url 'ftp://ftp.kde.org/pub/kde/stable/akonadi/src/akonadi-1.9.0.tar.bz2'
   md5 '51ef4c9455f97ce4346c5934174b9da8'
 
+  depends_on 'cmake' => :build
   depends_on 'shared-mime-info'
   depends_on 'soprano'
   depends_on 'boost'
