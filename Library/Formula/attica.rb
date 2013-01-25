35284193ed1540ee69c85896483f8287f8e7274d
Till Adam <till@kdab.com>
2013-01-25 18:16:30 +0100

    Properly depend on cmake.

--- Library/Formula/attica.rb
+++ Library/Formula/attica.rb
@@ -1,6 +1,9 @@
 require 'base_kde_formula'
 
 class Attica < BaseKdeFormula
+
+  depends_on 'cmake' => :build
+
   homepage 'http://www.kde.org/'
   url 'ftp://ftp.kde.org/pub/kde/stable/attica/attica-0.4.1.tar.bz2'
   md5 'b90983ec5d79e5ddcbc9146fa23cab72'
