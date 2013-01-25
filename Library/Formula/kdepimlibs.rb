e6db6956f281bbce4263dfbedb018c543c12ad1c
Till Adam <till@kdab.com>
2013-01-25 18:16:48 +0100

    Properly depend on cmake.

--- Library/Formula/kdepimlibs.rb
+++ Library/Formula/kdepimlibs.rb
@@ -10,6 +10,7 @@ class Kdepimlibs < BaseKdeFormula
     sha1 '1a88826f75143ad2f2ab76d1c434a643ba1db5be'
   end
 
+  depends_on 'cmake' => :build
   depends_on 'kdelibs'
   depends_on 'gpgme'
   depends_on 'akonadi'
