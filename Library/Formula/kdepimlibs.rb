a1e73bb5c0d30cbd48e78dd2394115850cdb8e61
Till Adam <till@kdab.com>
2013-02-10 20:50:29 +0100

    Bump to 4.10. Fix cmake dependency.

--- Library/Formula/kdepimlibs.rb
+++ Library/Formula/kdepimlibs.rb
@@ -2,8 +2,8 @@ require 'base_kde_formula'
 
 class Kdepimlibs < BaseKdeFormula
   homepage 'http://www.kde.org/'
-  url 'http://download.kde.org/stable/4.9.4/src/kdepimlibs-4.9.4.tar.xz'
-  sha1 '78fb5ec8d8a0cbd145f44d76618705c1e3e3b0a3'
+  url 'http://download.kde.org/stable/4.10.0/src/kdepimlibs-4.10.0.tar.xz'
+  sha1 'cf51d06ffd49de44312d5fae52b6fb7fb044459f'
 
   devel do
     url 'http://download.kde.org/unstable/4.9.95/src/kdepimlibs-4.9.95.tar.xz'
