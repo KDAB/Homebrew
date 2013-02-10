6d017c45b41990e1fb4532dcd7d104e01441d67a
Till Adam <till@kdab.com>
2013-02-10 16:23:31 +0100

    Bump to 4.10, add xz dependency.

--- Library/Formula/oxygen-icons.rb
+++ Library/Formula/oxygen-icons.rb
@@ -1,13 +1,15 @@
 require 'base_kde_formula'
 
+
 class OxygenIcons < BaseKdeFormula
   homepage 'http://www.oxygen-icons.org'
-  url 'http://download.kde.org/stable/4.9.4/src/oxygen-icons-4.9.4.tar.xz'
-  sha1 '459c0db3412e7f3aa3908467cbc6d9bc7af04c07'
+  url 'http://download.kde.org/stable/4.10.0/src/oxygen-icons-4.10.0.tar.xz'
+  sha1 'e7f9b5fd8fe0e6f6f9c0d744424134497328e65e'
 
   devel do
     url 'http://download.kde.org/unstable/4.9.95/src/oxygen-icons-4.9.95.tar.xz'
     sha1 'ab58b38d8d691d55578c5ad33ed0e5220f2906b1'
   end
 
+  depends_on 'xz' => :build
 end
