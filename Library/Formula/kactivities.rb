c9e27eb63cf43b16f3f4d5d77a4f279f98787a1b
Till Adam <till@kdab.com>
2013-02-13 20:51:10 +0100

    Update to 4.10.

--- Library/Formula/kactivities.rb
+++ Library/Formula/kactivities.rb
@@ -2,13 +2,14 @@ require 'base_kde_formula'
 
 class Kactivities < BaseKdeFormula
   homepage 'http://www.kde.org/'
-  url 'http://download.kde.org/stable/4.9.4/src/kactivities-4.9.4.tar.xz'
-  sha1 '1e3b69751d0da8416215038bc140748df23d4f37'
+  url 'http://download.kde.org/stable/4.10.0/src/kactivities-4.10.0.tar.xz'
+  sha1 '4d788f21019cbbe0c27ab3eef51a75125d9966b8'
 
   devel do
     url 'http://download.kde.org/unstable/4.9.95/src/kactivities-4.9.95.tar.xz'
     sha1 '2f908ab040f124bd054aff29fbe838378761a696'
   end
 
+  depends_on 'cmake' => :build
   depends_on 'kdelibs'
 end
